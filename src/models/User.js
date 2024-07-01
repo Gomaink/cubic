dffc006fe100f722ed6c9f8a05a4f59959d8caa3
Gomaink <muka5458@gmail.com>
2024-06-30 22:49:27 -0300

    User schema

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User "table" (SQL User lmfao)
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    nickname: { type: String, required: true },
+    password: { type: String, required: true },
+    birthdate: {
+        day: { type: Number, required: true },
+        month: { type: Number, required: true },
+        year: { type: Number, required: true }
+    }
+});
+
+// Bcrypt hash
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+//Bcrypt compare
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
