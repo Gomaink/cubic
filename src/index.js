7cd22a46ae54453492a3ef44bb103b48252220e2
Gomaink <muka5458@gmail.com>
2024-06-30 22:49:10 -0300

    express session & mongoose implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+//Express instance
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
+//Mongoose instance
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
+
+//Body-parser instance
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Express-session instance
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+//Static-files instance & EJS 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+// Routers
 const authenticationRouter = require('./routes/auth');
 app.use("/auth", authenticationRouter);
 
+// Routes
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -21,4 +41,5 @@ app.get("/another", (req, res) => {
     res.render("another-page");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}.`))
+//Express listener
+app.listen(port, () => console.log(`Server running on port ${port}.`));
