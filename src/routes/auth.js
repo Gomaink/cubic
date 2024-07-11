1383e94d82d3e73985812c6cab4101def3629ec1
Gomaink <muka5458@gmail.com>
2024-07-11 18:09:02 -0300

    forgot password fixes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -113,15 +113,31 @@ router.post('/forgot-password', async (req, res) => {
     const { email } = req.body;
 
     try {
+
         const user = await User.findOne({ email });
 
         if (!user) {
             return res.json({ error: 'E-mail não cadastrado.' });
         }
 
-        const token = crypto.randomBytes(20).toString('hex');
-        const expiration = Date.now() + 15 * 60 * 1000; // 15 minutos
+        // Verifica se existe um token ativo para o usuário
+        const existingToken = await Token.findOne({
+            userId: user._id,
+            expiration: { $gt: Date.now() }
+        });
 
+        if (existingToken) {
+            // Se existir, retorna um erro indicando que já há um pedido ativo
+            return res.json({ error: 'Já há um pedido de redefinição de senha ativo para este usuário.' });
+        }
+
+        // Deleta qualquer token expirado para o usuário
+        await Token.deleteMany({
+            userId: user._id,
+            expiration: { $lte: Date.now() }
+        });
+
+        // Salva o novo token no banco de dados
         await new Token({
             userId: user._id,
             token,
@@ -134,9 +150,52 @@ router.post('/forgot-password', async (req, res) => {
             from: 'seuemail@gmail.com',
             to: user.email,
             subject: 'Redefinir Senha',
-            html: `<p>Você solicitou uma redefinição de senha.</p>
-                   <p>Clique no link abaixo para redefinir sua senha:</p>
-                   <a href="${resetLink}">${resetLink}</a>`
+            html: `<!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Redefinição de Senha</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f0f0f0;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .container {
+                        max-width: 400px;
+                        margin: 20px auto;
+                        padding: 20px;
+                        background-color: #ffffff;
+                        border: 1px solid #cccccc;
+                        border-radius: 5px;
+                    }
+                    .btn {
+                        display: inline-block;
+                        font-weight: bold;
+                        padding: 10px 20px;
+                        text-decoration: none;
+                        background-color: #007bff;
+                        color: #ffffff;
+                        border-radius: 4px;
+                    }
+                    .btn:hover {
+                        background-color: #0056b3;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <h2 style="color: #000;">Redefinição de senha.</h2>
+                    <hr>
+                    <p style="color: #000; font-size: 16px;">Você solicitou uma redefinição de senha.</p>
+                    <p style="color: #000; font-size: 16px;">Caso não tenha sido você que solicitou, ignore este e-mail.</p>
+                    <p style="color: #000; font-size: 16px;">Clique no botão abaixo para redefinir sua senha:</p>
+                    <a href="${resetLink}" style="color: #fff;" class="btn">Alterar senha</a>
+                </div>
+            </body>
+            </html>`
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
@@ -151,6 +210,7 @@ router.post('/forgot-password', async (req, res) => {
         res.status(500).json({ error: 'Erro interno do servidor.' });
     }
 });
+
 
 router.get('/forgot-password/:token', async (req, res) => {
     const { token } = req.params;
@@ -199,7 +259,7 @@ router.post('/reset-password/:token', async (req, res) => {
 
         await Token.deleteOne({ _id: tokenDoc._id });
 
-        res.redirect('/auth/login', {error: 'Senha alterada com sucesso.'});
+        res.status(200).redirect('/auth/login?error=Senha alterada com sucesso.');
     } catch (error) {
         console.error(error);
         return res.status(500).redirect('/error?error=500&errorMessage=Erro interno do servidor.');
